9cd24b1bb939012be02849e1eeb3076eddf28670
ilya.fursa <ilya.fursa@simbirsoft.com>
2023-11-09 00:56:03 +0400

    TOA-4. Adding Detekt configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jlleitschuh.gradle.ktlint") version ("11.0.0")
+    id("io.gitlab.arturbosch.detekt").version("1.23.3")
 }
