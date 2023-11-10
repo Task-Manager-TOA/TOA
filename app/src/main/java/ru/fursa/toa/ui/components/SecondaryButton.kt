e5cb7e7e576ab5d7691e287a2d3b408bf37cb34f
Ilya Fursa <71984790+ifv93@users.noreply.github.com>
2023-11-11 03:40:44 +0400

    TOA-9. Added primary button (#9)

---
+++ app/src/main/java/ru/fursa/toa/ui/components/SecondaryButton.kt
@@ -0,0 +1,58 @@
+package ru.fursa.toa.ui.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.tooling.preview.Preview
+import ru.fursa.toa.R
+import ru.fursa.toa.ui.theme.TOATheme
+
+@Composable
+fun SecondaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+
+    TextButton(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(dimensionResource(id = R.dimen.button_height)),
+        shape = RoundedCornerShape(50),
+    ) {
+        Text(text = text.toUpperCase(Locale.current))
+    }
+}
+
+@Preview(
+    name = "Night mode",
+    uiMode = UI_MODE_NIGHT_YES
+)
+@Preview(
+    name = "Day mode",
+    uiMode = UI_MODE_NIGHT_NO
+)
+@Composable
+fun SecondaryButtonPreview() {
+    TOATheme {
+        Surface {
+            SecondaryButton(
+                text = "Secondary Button",
+                onClick = { }
+            )
+        }
+    }
+}
