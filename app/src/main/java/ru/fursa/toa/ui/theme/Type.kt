cfab82f739bdce90d7913783ac98d2e89790f5cc
Ilya Fursa <71984790+ifv93@users.noreply.github.com>
2023-11-11 02:43:13 +0400

    TOA-7. Added app's fonts (#7)

--- app/src/main/java/ru/fursa/toa/ui/theme/Type.kt
+++ app/src/main/java/ru/fursa/toa/ui/theme/Type.kt
@@ -2,33 +2,41 @@ package ru.fursa.toa.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ru.fursa.toa.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+
+private val UrbanistExtraBold = FontFamily(Font(R.font.urbanist_extrabold))
+private val UrbanistSemiBold = FontFamily(Font(R.font.urbanist_semibold))
+private val UrbanistBold = FontFamily(Font(R.font.urbanist_bold))
+private val UrbanistMedium = FontFamily(Font(R.font.urbanist_medium))
+private val UrbanistLight = FontFamily(Font(R.font.urbanist_light))
+
+val TOATypography = Typography(
+    headlineLarge = TextStyle(
+        fontFamily = UrbanistExtraBold,
+        fontSize = 40.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    headlineMedium = TextStyle(
+        fontFamily = UrbanistExtraBold,
+        fontSize = 35.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = UrbanistSemiBold,
+        fontSize = 13.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = UrbanistMedium,
+        fontSize = 15.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = UrbanistLight,
+        fontSize = 13.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = UrbanistBold,
+        fontSize = 13.sp
     )
-    */
 )
