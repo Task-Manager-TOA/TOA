48b366c43abe37c92ae80226db109370c4f5c9d6
ilya.fursa <ilya.fursa@simbirsoft.com>
2023-11-11 03:37:50 +0400

    TOA-9. Added secondary button

--- app/src/main/java/ru/fursa/toa/ui/components/SecondaryButton.kt
+++ app/src/main/java/ru/fursa/toa/ui/components/SecondaryButton.kt
@@ -5,11 +5,10 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,25 +20,18 @@ import ru.fursa.toa.R
 import ru.fursa.toa.ui.theme.TOATheme
 
 @Composable
-fun PrimaryButton(
+fun SecondaryButton(
     text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    backgroundColor: Color = MaterialTheme.colorScheme.primary,
-    textColor: Color = MaterialTheme.colorScheme.onPrimary
 ) {
-    val buttonColors = buttonColors(
-        containerColor = backgroundColor,
-        contentColor = textColor
-    )
 
-    Button(
+    TextButton(
         onClick = onClick,
         modifier = modifier
             .fillMaxWidth()
             .height(dimensionResource(id = R.dimen.button_height)),
         shape = RoundedCornerShape(50),
-        colors = buttonColors
     ) {
         Text(text = text.toUpperCase(Locale.current))
     }
@@ -54,11 +46,11 @@ fun PrimaryButton(
     uiMode = UI_MODE_NIGHT_NO
 )
 @Composable
-fun PrimaryButtonPreview() {
+fun SecondaryButtonPreview() {
     TOATheme {
         Surface {
-            PrimaryButton(
-                text = "Primary Button",
+            SecondaryButton(
+                text = "Secondary Button",
                 onClick = { }
             )
         }
