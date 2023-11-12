9fde754924b3e0ef0bea4e604fe1c2b005a75745
ilya.fursa <ilya.fursa@simbirsoft.com>
2023-11-13 02:43:14 +0400

    TOA-13. Mock login with credentials process

--- app/src/main/java/ru/fursa/toa/login/domain/usecase/LoginUseCase.kt
+++ app/src/main/java/ru/fursa/toa/login/domain/usecase/LoginUseCase.kt
@@ -2,6 +2,7 @@ package ru.fursa.toa.login.domain.usecase
 
 import ru.fursa.toa.core.data.DataResponse
 import ru.fursa.toa.login.domain.model.LoginResult
+import ru.fursa.toa.login.domain.model.LoginType
 
 
 @JvmInline
@@ -11,5 +12,5 @@ value class Email(val email: String)
 value class Password(val password: String)
 
 interface LoginUseCase {
-    suspend fun login(email: Email, password: Password): LoginResult
+    suspend fun login(loginType: LoginType): LoginResult
 }
