84f520b7d65c8bcdf8071b739fd669f0af336249
ilya.fursa <ilya.fursa@simbirsoft.com>
2023-11-15 02:47:15 +0400

    TOA-13. Login use case unit testing added

--- app/src/main/java/ru/fursa/toa/core/data/DataResponse.kt
+++
@@ -1,6 +0,0 @@
-package ru.fursa.toa.core.data
-
-sealed class DataResponse<out T> {
-    data class Success<out T>(val data: T): DataResponse<T>()
-    data class Error<out T>(val error: Throwable): DataResponse<T>()
-}
